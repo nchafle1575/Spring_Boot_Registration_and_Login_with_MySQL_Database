d46d1265b7a2b684f748ce529177ebf5d7518515
nchafle1575 <92887229+nchafle1575@users.noreply.github.com>
2021-12-02 11:09:06 +0530

    Add files via upload
    Through this Spring Boot tutorial, I’d love to guide you to develop a Java web application from scratch with the essential features: user registration, login, logout, and view users list – with user information stored in MySQL database. You will go through the process of coding a Spring Boot project using the following technologies:
    1) Spring Web MVC for the web layer
    2) Spring Data JPA with Hibernate framework or the data access layer
    3) Spring Security for authentication, login and logout
    4) Thymeleaf as template engine
    5) HTML 5 and Bootstrap 4 for responsive user interface
    6) JUnit 5 and AssertJ for unit testing
    7) MySQL database

---
+++ loginapp/loginapp/src/main/java/com/loginapp/loginapp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.loginapp.loginapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.loginapp.loginapp.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+}
